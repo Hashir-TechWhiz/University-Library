e65ec8d90083def83ddbe169381740246628ac2c
Muhammad Hashir <hashirmohamed04@gmail.com>
2025-01-24 09:35:24 +0530

    Workflow Email sending

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,11 +2,36 @@ import { auth } from "@/auth";
 import { ReactNode } from "react";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session) redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    //get the user and see if the last activity date is today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.user.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({
+        lastActivityDate: new Date().toISOString().slice(0, 10),
+      })
+      .where(eq(users.id, session?.user?.id));
+  });
 
   return (
     <main className="flex min-h-screen flex-1 flex-col bg-pattern bg-cover bg-top bg-dark-100 px-5 xs:px-10 md:px-16">
